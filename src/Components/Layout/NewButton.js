0c7a5c71045c368bc1fba61df3b9cac9d8a0f790
iamrobins <itsrobin4@gmail.com>
2021-06-10 20:40:28 +0530

    Download Limit Added

--- src/Components/Layout/NewButton.js
+++ src/Components/Layout/NewButton.js
@@ -1,15 +1,41 @@
-import { Tooltip, Button } from "@chakra-ui/react";
+import { Tooltip, Button, useToast } from "@chakra-ui/react";
 import { FiPlus } from "react-icons/fi";
 import { useDispatch } from "react-redux";
 import { UPLOADS_ON } from "State/constants";
 
 export default function NewButton() {
+  const toast = useToast();
   const dispatch = useDispatch();
 
   const updateUploadManager = e => {
     const files = [...e.target.files];
+    if (files.length > 20) {
+      return toast({
+        title: `You can't upload more than 20 files at once`,
+        status: "error",
+        isClosable: true,
+      });
+    }
+    //Prevent large files from uploading
+    const updatedFiles = files.filter(file => {
+      if (file.size > 1.01e8) {
+        toast({
+          title: `${file.name} is removed due to large size !`,
+          status: "warning",
+          isClosable: true,
+        });
+        toast({
+          title: `Max Limit is 100MB`,
+          status: "success",
+          isClosable: true,
+        });
+        return null;
+      }
+      return file;
+    });
+    if (updatedFiles.length === 0) return;
 
-    dispatch({ type: UPLOADS_ON, payload: files });
+    dispatch({ type: UPLOADS_ON, payload: updatedFiles });
   };
 
   return (
